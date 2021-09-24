9b5a45ed02fd4236f0439e75c0c4f194b901875d
AJ <abdullah141299@gmail.com>
2021-09-24 23:34:46 +0100

    Reverted

--- components/FAQ/FaqParser.js
+++
@@ -1,9 +0,0 @@
-const FaqParser = () => {
-  // const faqData = require("../../public/assets/faq.json");
-  // console.log(gitData);
-  // console.log("LINE BREAK");
-  // console.log(faqData);
-  return (<div></div>);
-};
-
-export default FaqParser;
