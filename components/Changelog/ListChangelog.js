042626917ab55b34d09475ae68d5a6e07ffde914
AJ <abdullah141299@gmail.com>
2021-09-24 10:21:37 +0100

    modified changelog page design

--- components/Changelog/ListChangelog.js
+++ components/Changelog/ListChangelog.js
@@ -15,17 +15,17 @@ const ListChangelog = ({ gitData }) => {
       console.log("Changelog API not working!");
     }
   }
+
   return (
     <div>
       { gitData.map((data) => {
         return (
           <div className="first:text-pink-500 text-gray-700 dark:text-gray-400 mb-10 w-[75%] " key={ data.id }>
-            <h1 className="font-semibold text-2xl">{ data.tag_name }</h1>
+            <h1 className="font-semibold text-2xl">{ data.tag_name } <span className="ml-2 text-lg font-normal">({ gitData[0].published_at.split('T')[0] })</span></h1>
             <div className="">
               <ChangelogParser data={ data } />
             </div>
-            <div className="w-full border-b my-3 border-gray-400 dark:border-gray-600 fill-current"></div>
-            <div className="flex space-x-2 mt-2 items-center">
+            <div className="flex space-x-2 mt-3 items-center">
               <a href={ data.assets[0].browser_download_url }>
                 <button className="flex items-center space-x-1 px-5 py-[6px] text-sm bg-gray-700 dark:bg-gray-400 text-gray-200 dark:text-gray-900 hover:bg-black dark:hover:bg-gray-300 transition duration-200 rounded-full">
                   <HiDownload size="17px" />
@@ -33,12 +33,13 @@ const ListChangelog = ({ gitData }) => {
                 </button>
               </a>
               <a href={ data.html_url } target="_blank">
-                <button className="flex items-center space-x-1  px-5 py-[6px] text-sm bg-gray-700 dark:bg-gray-800 text-gray-200 dark:text-gray-300 hover:bg-black dark:hover:bg-gray-700  transition duration-200 rounded-full">
+                <button className="flex items-center space-x-1 px-5 py-[6px] text-sm bg-gray-700 dark:bg-gray-800 text-gray-200 dark:text-gray-300 hover:bg-black dark:hover:bg-gray-700  transition duration-200 rounded-full">
                   <FaGithub size="17px" />
                   <span>GitHub Release</span>
                 </button>
               </a>
             </div>
+            <div className="w-full border-b my-3 border-gray-400 dark:border-gray-600 fill-current"></div>
           </div>
         );
       }) }
