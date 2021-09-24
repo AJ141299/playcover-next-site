7c853a88fb9f04d71db144187d4c337adf2afde0
AJ <abdullah141299@gmail.com>
2021-09-24 05:28:33 +0100

    added react icons pack

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-900 text-gray-200">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
