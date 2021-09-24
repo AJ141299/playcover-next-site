49d3e232f3a99fe95cd8fa4bc5ccbb3c4e327801
AJ <abdullah141299@gmail.com>
2021-09-24 23:36:33 +0100

    test

---
+++ components/FAQ/FaqParser.js
@@ -0,0 +1,9 @@
+const FaqParser = () => {
+  // const faqData = require("../../public/assets/faq.json");
+  // console.log(gitData);
+  // console.log("LINE BREAK");
+  // console.log(faqData);
+  return (<div></div>);
+};
+
+export default FaqParser;
